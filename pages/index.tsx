dffa51c200d54595dc5f22b73e0a49eec3841756
jaehyuenjung <jaehyuen0613@gmail.com>
2022-08-26 14:05:05 +0900

    프로젝트 셋업 - 에셋 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-    return null;
+    return (
+        <div>
+            <a className="absolute w-32 h-6">
+                <Image
+                    src="/sungkyulin.svg"
+                    alt="Sungkyulin Logo"
+                    layout="fill"
+                />
+            </a>
+        </div>
+    );
 };
 
 export default Home;
